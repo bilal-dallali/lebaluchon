e7449834fc0a3497c8e7344dc7d3c5ef46539fd1
bilal-dallali <dallali-bilal@hotmail.fr>
2024-06-26 22:33:35 +0200

    done module

--- LeBaluchon/CurrencyViewController.swift
+++ LeBaluchon/CurrencyViewController.swift
@@ -9,13 +9,17 @@ import UIKit
 
 class CurrencyViewController: UIViewController {
     
-    //@IBOutlet weak var currencyTitle: UILabel!
-    
-    @IBOutlet var currencyTitle: UILabel!
+    @IBOutlet weak var currencyTitle: UILabel!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        currencyTitle.font = UIFont(name: "PlusJakartaSans-Bold", size: 28)
+        if currencyTitle != nil {
+            print("currencyTitle outlet is connected")
+        } else {
+            print("currencyTitle outlet is NOT connected")
+        }
+        
+        currencyTitle.font = UIFont(name: "PlusJakartaSans-Bold", size: 28) ?? UIFont.systemFont(ofSize: 28, weight: .bold)
     }
 }
