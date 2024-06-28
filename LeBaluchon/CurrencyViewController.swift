285bb57f78a343e9b79e7c72d2eab7282e184eea
bilal-dallali <dallali-bilal@hotmail.fr>
2024-06-28 16:31:49 +0200

    continue currency view

--- LeBaluchon/CurrencyViewController.swift
+++ LeBaluchon/CurrencyViewController.swift
@@ -10,10 +10,21 @@ import UIKit
 class CurrencyViewController: UIViewController {
     
     @IBOutlet var currencyTitle: UILabel!
+    @IBOutlet var currencyMainView: UIView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        // FONTS FOR THE APPLICATION
         currencyTitle.font = UIFont(name: "PlusJakartaSans-Bold", size: 28)
+        
+        
+        // SHADOWS
+        currencyMainView.layer.shadowColor = UIColor.white.cgColor
+        currencyMainView.layer.shadowOpacity = 0.1
+        currencyMainView.layer.shadowOffset = .init(width: -4, height: -4)
+        currencyMainView.layer.shadowRadius = 4
+        
+        
     }
 }
