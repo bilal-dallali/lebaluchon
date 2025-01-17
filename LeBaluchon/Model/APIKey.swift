a593b03e444fa58b7997d5bb65b0adfa9098f990
bilal-dallali <dallali-bilal@hotmail.fr>
2025-01-17 18:58:04 +0100

    delete api keys

--- LeBaluchon/Model/APIKey.swift
+++ LeBaluchon/Model/APIKey.swift
@@ -7,6 +7,6 @@
 
 import Foundation
 
-let weatherApiKey = "07bf46530bf88149822e9ff3fabf4bea"
-let currencyApiKey = "9be330ea6bc0ad1ca77da0c4747cd295"
-let googleTranslateApiKey = "AIzaSyAXq-QaKulAeS_ThbhPtBwoPc4ZvvWEg6Q"
+let weatherApiKey = ""
+let currencyApiKey = ""
+let googleTranslateApiKey = ""
