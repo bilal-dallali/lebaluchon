0aed31af04842e869729da2e497b668f7e90cf80
bilal-dallali <dallali-bilal@hotmail.fr>
2025-01-20 11:57:06 +0100

    adding tests no data weather

--- LeBaluchonTests/WeatherTests.swift
+++ LeBaluchonTests/WeatherTests.swift
@@ -22,13 +22,13 @@ final class WeatherManagerTests: XCTestCase {
     
     func testParseJSONValidData() throws {
         let json = """
-            {
-                "weather": [{"id": 800, "description": "clear sky"}],
-                "main": {"temp": 22.5},
-                "name": "Paris",
-                "timezone": 3600
-            }
-            """.data(using: .utf8)!
+        {
+            "weather": [{"id": 800, "description": "clear sky"}],
+            "main": {"temp": 22.5},
+            "name": "Paris",
+            "timezone": 3600
+        }
+        """.data(using: .utf8)!
         
         let weather = weatherManager.parseJSON(weatherData: json)
         
@@ -37,5 +37,32 @@ final class WeatherManagerTests: XCTestCase {
         XCTAssertEqual(weather?.townName, "Paris", "Town name does not match")
         XCTAssertEqual(weather?.temperature, 22.5, "Temperature does not match")
         XCTAssertEqual(weather?.timezone, 3600, "Timezone does not match")
+    }
+    
+    func testParseJSONNoData() throws {
+        let json = "".data(using: .utf8)!
+        let weather = weatherManager.parseJSON(weatherData: json)
+        XCTAssertNil(weather, "WeatherModel should be nil for empty JSON")
+    }
+    
+    func testParseJSONInvalidData() throws {
+        let json = """
+        {
+            "invalidKey": "invalidValue"
+        }
+        """.data(using: .utf8)!
+        let weather = weatherManager.parseJSON(weatherData: json)
+        XCTAssertNil(weather, "WeatherModel should be nil for invalid JSON structure")
+    }
+    
+    func testParseJSONMissingFields() throws {
+        let json = """
+        {
+            "main": {"temp": 22.5},
+            "timezone": 3600
+        }
+        """.data(using: .utf8)!
+        let weather = weatherManager.parseJSON(weatherData: json)
+        XCTAssertNil(weather, "WeatherModel should be nil if required fields are missing")
     }
 }
